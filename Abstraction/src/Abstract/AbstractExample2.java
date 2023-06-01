6e7416ab96f1b39488f87cfa18867adcaaa54a74
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-06-02 01:58:19 +0530

    Add files via upload

---
+++ Abstraction/src/Abstract/AbstractExample2.java
@@ -0,0 +1,23 @@
+package Abstract;
+
+abstract class Shape{
+	abstract void draw(); 
+}
+class Rectangle extends Shape{
+	void draw() {
+		System.out.println("Drwaing rectangle");
+	}
+}
+class Circle1 extends Shape{
+	void draw() {
+		System.out.println("Drawing Cicle");
+	}
+}
+class AbstractExxample2{
+	public static void main(String[] args) {
+		Shape r= new Rectangle();
+		r.draw();
+		Shape s =new Circle1();
+		s.draw();
+	}
+}
