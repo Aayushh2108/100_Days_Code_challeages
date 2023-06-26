7b98c8060ffa049519e6480de43c2490c5e2d833
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-06-27 02:30:47 +0530

    SqureInput.cpp

---
+++ SqureInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter the number"<<endl;
+    
+    cin>>x;
+    cout<<"Square of number is ";
+    cout<<x*x;
+
+    return 0;
+}
