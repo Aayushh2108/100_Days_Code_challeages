dc8ce517ad81c5215801dcd2cf4cd04e008d8c1e
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-06-29 23:19:32 +0530

    Constructor3.java

--- Constructor3.java
+++ Constructor3.java
@@ -12,7 +12,7 @@ class Student4{
     public static void main(String args[]){  
     
     Student4 s1 = new Student4(111,"Karan");  
-    Student4 s2 = new Student4(222,"Aryan") 
+    Student4 s2 = new Student4(222,"Aryan") ;
     //calling method to display the values of object  
     s1.display();  
     s2.display();  
