c8d8ddc23f60f11d8d3a0f323bd9139a9dfb2098
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-08-01 22:38:25 +0530

    FactorialExample.java

--- FactorialExample.java
+++ FactorialExample.java
@@ -1,5 +1,5 @@
 class FactorialExample{  
- public static void main(String args[]) 
+ public static void main(String args[]) {
   int i,fact=1;  
   int number=5;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
