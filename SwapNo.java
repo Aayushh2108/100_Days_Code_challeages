feb2374339ce0654476fcfa56ef6a177ecdcdee2
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-08-09 17:07:10 +0530

    Create SwapNo.java

---
+++ SwapNo.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+class SwapNo {
+
+
+	static void swapValuesUsingThirdVariable(int m, int n)
+	{
+		// Swapping the values
+		int temp = m;
+		m = n;
+		n = temp;
+		System.out.println("Value of m is " + m
+						+ " and Value of n is " + n);
+	}
+
+	// Main driver code
+	public static void main(String[] args)
+	{
+		// Random integer values
+		int m = 9, n = 5;
+
+
+		swapValuesUsingThirdVariable(m, n);
+	}
+}
