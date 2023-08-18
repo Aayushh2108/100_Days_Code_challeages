9ee5401a40acb5979d9844ce4c0ee503e6b50c0b
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-08-18 20:11:04 +0530

    Two_Matrix.java

--- Two_Matrix.java
+++ Two_Matrix.java
@@ -8,7 +8,7 @@ class Two_Matrix {
 	
 	static void printMatrix(int M[][],
 							int rowSize,
-							int colSize)
+							int colSize,
 	{
 		for (int i = 0; i < rowSize; i++) {
 			for (int j = 0; j < colSize; j++)
