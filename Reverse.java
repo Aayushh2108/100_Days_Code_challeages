199feff461a2b6cc389718a2e04a6d535ac863bf
Aayush <119883228+Aayushh2108@users.noreply.github.com>
2023-11-24 23:30:20 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -2,7 +2,7 @@ package Maths;
 
 public class Reverse {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
 		String name = "Aayush";
 		int leng =name.length();
 		String rev = "";
